6c0154614a5be9b773abdc25289fb21ea05340cf
Benjamin Frost <bf@xhs.technology>
2019-03-04 14:47:02 +1100

    Implements UITableViewDataSource in CityWeatherViewController.

--- WeatherChamp/CityWeatherViewController.swift
+++ WeatherChamp/CityWeatherViewController.swift
@@ -8,23 +8,36 @@
 
 import UIKit
 
-class CityWeatherViewController: UIViewController {
+class CityWeatherViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let cellIdentifier = "cityCell"
+    
+    @IBOutlet weak var tableView: UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - UITableViewDelegate/DataSource Implementation
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+            as? CityTableViewCell else {
+                return UITableViewCell()
+        }
+        
+        // TODO: Apply data to cell
+        
+        return cell
+    }
+    
 
 }
