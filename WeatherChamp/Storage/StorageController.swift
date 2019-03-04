eaf341a5ac54f53cf41706f9216570173a1d6ed2
Benjamin Frost <bf@xhs.technology>
2019-03-04 16:12:38 +1100

    Adds StorageController protocol, to abstract away the saving/loading.

---
+++ WeatherChamp/Storage/StorageController.swift
@@ -0,0 +1,21 @@
+//
+//  StorageController.swift
+//  WeatherChamp
+//
+//  Created by Benjamin Frost on 4/3/19.
+//  Copyright © 2019 Benjamin Frost. All rights reserved.
+//
+
+import Foundation
+
+
+
+protocol StorageController {
+    
+    // These methods could be upgraded to return Promises or handle
+    // asynchronous cases if needed, but this will do for now.
+    
+    func save(city: Model.City) throws
+    func delete(city: Model.City) throws
+    func getCities() throws -> [Model.City] 
+}
